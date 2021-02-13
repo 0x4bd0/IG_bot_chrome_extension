d3c803daba6f72dbc0596e25e66eb9f5c54e0192
0x4bd0 <abdoumjr@gmail.com>
2021-02-13 12:33:27 +0100

    added a way to stop the boting from liking on ig exit button

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,17 +5,22 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
 		let myHash = window.location.hash
 
 		if(myHash === "#igautomatebot"){
 
 			let myItems = document.querySelectorAll('.SCxLW a')
 			myItems[0].click()
-				setInterval(()=>{
+
+			const bot = setInterval(()=>{
 					document.querySelector('.ltpMr .wpO6b').click()		
 					document.querySelector('.coreSpriteRightPaginationArrow ').click()
 				},3000)
+
+	  document.querySelector('body > div._2dDPU.CkGkG > div.Igw0E.IwRSH.eGOV_._4EzTm.BI4qX.qJPeX.fm1AK.TxciK.yiMZG > button').onclick = () => {
+		  clearInterval(bot)
+	  }
+
 
 		}
 		// ----------------------------------------------------------
